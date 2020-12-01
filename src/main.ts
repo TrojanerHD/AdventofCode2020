3974cc887a65c92cf53f6f577141cdf2fbf9c570
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2020-12-01 06:24:42 +0100

    Reformat of main file

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,36 @@
-export interface Log {message: string; value: string}
+export interface Log {
+  message: string;
+  value: string;
+}
 export type Response = Array<Log> | undefined;
 export class Day {
-    private _day: string;
+  private _day: string;
 
-    constructor(day: number) {
-        this._day = day.toString();
-        if(day <= 9) this._day = `0${this._day}`;
-        this.readValues();
-    }
+  constructor(day: number) {
+    this._day = day.toString();
+    if (day <= 9) this._day = `0${this._day}`;
+    this.readValues();
+  }
 
-    async readValues(): Promise<void> {
-        Deno.chdir(`./src/${this._day}`);
-        const execute: (file: string) => Response = await import(`./${this._day}/index`);
-        const file: string = await Deno.readTextFile(`./src/${this._day}/values.txt`);
-        const response: Response = execute(file);
-        Deno.chdir('../../');
-        for (let i = 0; i < response!.length; i++) {
-            const log: Log = response![i];
-            this.logger(i + 1, log.message, log.value)
-        }
+  async readValues(): Promise<void> {
+    Deno.chdir(`./src/${this._day}`);
+    const execute: { main: (data: string) => Response } = await import(
+      `./${this._day}/index.ts`
+    );
+    const file: string = await Deno.readTextFile(`./values.txt`);
+    const response: Response = execute.main(file);
+    Deno.chdir("../../");
+    for (let i = 0; i < response!.length; i++) {
+      const log: Log = response![i];
+      this.logger(i + 1, log.message, log.value);
     }
+  }
 
-    private logger(part: number, message: string, value: string) {
-        console.log(`[Year 2020, Day ${this._day}, Part: ${part}] ${message}: ${value}`);
-    }
+  private logger(part: number, message: string, value: string) {
+    console.log(
+      `[Year 2020, Day ${this._day}, Part: ${part}] ${message}: ${value}`
+    );
+  }
 }
+
+new Day(1);
