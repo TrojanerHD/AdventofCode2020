2af5e03fd77bf86aa70fe498534c6f1d28a61122
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2020-12-01 06:25:02 +0100

    Added solution for day 01

---
+++ src/01/index.ts
@@ -0,0 +1,32 @@
+import { Response } from "../main.ts";
+
+export function main(data: string): Response {
+  const lines: string[] = data.split("\n");
+  let outputFirstSolution: number | undefined = undefined;
+  let outputSecondSolution: number | undefined = undefined;
+  for (const value1 of lines) {
+    for (const value2 of lines) {
+      if (+value1 + +value2 === 2020) outputFirstSolution = +value1 * +value2;
+
+      if (outputSecondSolution !== undefined) continue;
+      if (outputFirstSolution !== undefined) break;
+      for (const value3 of lines) {
+        if (+value1 + +value2 + +value3 === 2020) {
+          outputSecondSolution = +value1 * +value2 * +value3;
+          break;
+        }
+      }
+    }
+    if (outputFirstSolution !== undefined) break;
+  }
+  return [
+    {
+      message: "Entries that sum up to 2020 multiplied",
+      value: outputFirstSolution!.toString(),
+    },
+    {
+      message: "Three entries that sum up to 2020 multiplied",
+      value: outputSecondSolution!.toString(),
+    },
+  ];
+}
