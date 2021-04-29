2a945ce68a26edddefa103b9f8ce334ef792cb37
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-04-29 14:50:59 +0200

    Rewrote day 13 part 2; it still takes hours for the whole input but is a lot faster in the shorter examples

--- src/13/index.ts
+++ src/13/index.ts
@@ -10,13 +10,13 @@ export interface BusTimestamp {
 
 export default class Day13 implements Day {
   private _busTimestamps: BusTimestamp[] = [];
-  private _workerCount: number = 0;
+  private _workerCount = 0;
   private _workers: Worker[] = [];
   private _workerValues: { i: number; count: number }[] = [];
 
   main(data: string): Response {
     const lines: string[] = data.split(/\r?\n/g);
-    const myTimestamp: number = Number(lines[0]);
+    const myTimestamp = Number(lines[0]);
     for (const timestamp of lines[1].split(',')) {
       if (timestamp === 'x') continue;
       this._busTimestamps.push({
@@ -48,7 +48,7 @@ export default class Day13 implements Day {
         current: parsedTimestamp,
       });
     }
-    this.initWorkers(100000000000000);
+    const partTwo: number = this.partTwo();
     return [
       {
         message: 'Number of minutes multiplied with bus id',
@@ -56,14 +56,41 @@ export default class Day13 implements Day {
       },
       {
         message: 'First time the busses depart in a row',
-        value: this._busTimestamps.length + 1,
+        value: partTwo,
       },
     ];
   }
 
+  private partTwo(): number {
+    let largestTimestampPosition = 0;
+    const timestamps: {timestamp: number, offset: number}[] = [];
+    for (let i = 0; i < this._busTimestamps.length; i++) {
+      const timestamp: Timestamp = this._busTimestamps[i].start;
+      if (timestamp === 'x') continue;
+      timestamps.push({timestamp, offset: i});
+    }
+    timestamps.sort((a: {timestamp: number, offset: number}, b: {timestamp: number, offset: number}) => a.timestamp < b.timestamp ? 1 : -1);
+    for (let i = 1; i < this._busTimestamps.length; i++) {
+      const timestamp: Timestamp = this._busTimestamps[i].start;
+      if (timestamp !== 'x' && timestamp > this._busTimestamps[largestTimestampPosition].start) largestTimestampPosition = i;
+    }
+    let i = 0;
+    const largestTimestamp: Timestamp = this._busTimestamps[largestTimestampPosition].start;
+    if (largestTimestamp === 'x') return 0;
+    while (!this.recursivePartTwo(0, i - largestTimestampPosition, timestamps)) i += largestTimestamp;
+    return i - largestTimestampPosition;
+  }
+
+  private recursivePartTwo(i: number, init: number, timestamps: {timestamp: number, offset: number}[]): boolean {
+    if (i >= timestamps.length) return true;
+    const timestamp = timestamps[i];
+    if ((timestamp.offset + init) % timestamp.timestamp === 0) return this.recursivePartTwo(i + 1, init, timestamps);
+    return false;
+  }
+
   private countUp(minNumber: number): number {
     if (minNumber <= 0) return minNumber;
-    let minCount: number = Infinity;
+    let minCount = Infinity;
     for (const busTimestamp of this._busTimestamps) {
       let count: number = minNumber;
       if (busTimestamp.current === 'x' || busTimestamp.start === 'x') continue;
@@ -72,124 +99,5 @@ export default class Day13 implements Day {
       if (count < minCount) minCount = count;
     }
     return minCount;
-  }
-
-  private workerMessage(e: MessageEvent<any>): void {
-    this._workerCount--;
-    const properties: { i: number; count: number } = JSON.parse(e.data);
-    this._workerValues.push(properties);
-    if (this._workerCount !== 0) return;
-    this._workerValues.sort(
-      (a: { i: number; count: number }, b: { i: number; count: number }) =>
-        a.i > b.i ? 1 : -1
-    );
-    for (let i: number = 0; i < this._busTimestamps.length; i++) {
-      if (this._busTimestamps[i].current === 'x') continue;
-      this.countUp(this._workerValues[i].count - 1);
-      break;
-    }
-    let first: BusTimestamp = { current: -1, start: -1 };
-    let skip: boolean = false;
-    for (let i = 0; i < this._busTimestamps.length; i++) {
-      const current: BusTimestamp = this._busTimestamps[i];
-      if (current.current === 'x') continue;
-      if (first.current === -1) {
-        first.current = current.current;
-        first.start = current.start;
-      }
-      if (first.current === 'x') continue;
-      if (current.current - first.current !== i) {
-        skip = true;
-        break;
-      }
-    }
-    if (!skip) {
-      console.log(`Found ${first.current}`);
-      for (const worker of this._workers) worker.terminate();
-    } else {
-      for (let i: number = 0; i < this._workers.length; i++) {
-        this._workerCount++;
-        const worker: Worker = this._workers[i];
-        const workerValues: { i: number; count: number } = this._workerValues[
-          i
-        ];
-        if (first.start === 'x')
-          throw new Error('first start is x even though it never should');
-        workerValues.count += first.start;
-        worker.postMessage(
-          `{"i": ${workerValues.i}, "count": ${
-            workerValues.count
-          }, "busTimestamps": ${JSON.stringify(this._busTimestamps)} }`
-        );
-      }
-      this._workerValues = [];
-    }
-  }
-
-  private initWorkers(count: number): void {
-    this._workerCount += this._busTimestamps.length - 1;
-    for (let i = 1; i < this._busTimestamps.length; i++) {
-      const worker = new Worker(
-        new URL('bruteForce.ts', import.meta.url).href,
-        { type: 'module', deno: true }
-      );
-      worker.onmessage = this.workerMessage.bind(this);
-      this._workers.push(worker);
-      worker.postMessage(
-        `{"i": ${i}, "count": ${count}, "busTimestamps": ${JSON.stringify(
-          this._busTimestamps
-        )}}`
-      );
-    }
-  }
-
-  private bruteForce(i: number, count: number): boolean {
-    const timestamp: BusTimestamp = this._busTimestamps[i];
-    let previousTimestamp: BusTimestamp | undefined;
-    let lastValue: number = i - 1;
-    for (let j: number = i - 1; j >= 0; j--) {
-      if (this._busTimestamps[j].current === 'x') continue;
-      lastValue = j;
-      break;
-    }
-    previousTimestamp = this._busTimestamps[lastValue];
-    if (
-      timestamp.start === 'x' ||
-      previousTimestamp.start === 'x' ||
-      previousTimestamp.current === 'x'
-    )
-      return true;
-    if (
-      (count - i + lastValue) % previousTimestamp.start === 0 &&
-      count % timestamp.start === 0
-    ) {
-      //this.countUp(count);
-      return true;
-    }
-    return false;
-  }
-  //Stolen from https://stackoverflow.com/a/31302607/9634099
-  private leastCommonMultiple(min: number, max: number) {
-    function range(min: number, max: number): number[] {
-      const arr: number[] = [];
-      for (let i: number = min; i <= max; i++) arr.push(i);
-
-      return arr;
-    }
-
-    function gcd(a: number, b: number): number {
-      return !b ? a : gcd(b, a % b);
-    }
-
-    function lcm(a: number, b: number): number {
-      return (a * b) / gcd(a, b);
-    }
-
-    let multiple: number = min;
-    range(min, max).forEach((n: number): void => {
-      multiple = lcm(multiple, n);
-    });
-
-    return multiple;
   }
 }
